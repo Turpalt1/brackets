ad3a09cf22e61929cbffb6430cf16faa2e7d5ea8
Turpal <voin_957@mail.ru>
2024-08-22 00:22:37 +0300

    feat: fiiiinished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 !== 0) return false;
+  let res = 0;
+  let arr = str.split(''); 
+  while(arr.length !== 0 && res <= 100){
+    res++
+    bracketsConfig.forEach(e => {
+      for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i],arr[i + 1],arr)
+        if(arr[i] === e[0] && arr[i + 1] === e[1]){
+          arr.splice(i + 1,1);
+          arr.splice(i, 1);
+          console.log('win');
+          break;
+        }
+      }
+    })
+  }
+  return arr.length === 0;
 }
